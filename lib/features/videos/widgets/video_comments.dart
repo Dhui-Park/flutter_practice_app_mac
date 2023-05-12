43e72b0e444a3ac016662c17b2a9265e3c229aa4
dhui <dhui@bagchaejin-ui-MacBookAir.local>
2023-05-12 15:33:32 +0900

    Comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/constants/gaps.dart';
 import 'package:flutter_practice/constants/sizes.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -34,10 +35,70 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
           itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            child: const Text('I am a comment.'),
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                child: Text('nico'),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'nico',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size14,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    Gaps.v3,
+                    const Text(
+                        "That's not it I've seen the same thing but also in a cave."),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    color: Colors.grey.shade500,
+                    size: Sizes.size20,
+                  ),
+                  Gaps.v2,
+                  Text(
+                    '52.2K',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: const Text('nico'),
+              ),
+            ],
           ),
         ),
       ),
