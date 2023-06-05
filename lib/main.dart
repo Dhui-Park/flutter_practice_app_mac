14fcf99558768103ea812b8b6267bf371cef28f3
dhui <dhui@bagchaejin-ui-MacBookAir.local>
2023-06-05 23:16:29 +0900

    ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/features/common/widgets/main_navigation/widgets/video_configuration/video_config.dart';
 import 'package:flutter_practice/router.dart';
 
 import 'constants/sizes.dart';
@@ -13,29 +12,27 @@ class FlutterPracticeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return VideoConfig(
-      child: MaterialApp.router(
-        routerConfig: router,
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Practice',
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            centerTitle: true,
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.white,
-            elevation: 0,
-            titleTextStyle: TextStyle(
-              color: Colors.black,
-              fontSize: Sizes.size16 + Sizes.size2,
-              fontWeight: FontWeight.w600,
-            ),
+    return MaterialApp.router(
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Practice',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
           ),
-          splashColor: Colors.transparent,
-          scaffoldBackgroundColor: Colors.white,
-          primaryColor: const Color(0xFFCFB997),
-          textSelectionTheme: const TextSelectionThemeData(
-            cursorColor: Color(0xFFCFB997),
-          ),
+        ),
+        splashColor: Colors.transparent,
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: const Color(0xFFCFB997),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(0xFFCFB997),
         ),
       ),
     );
