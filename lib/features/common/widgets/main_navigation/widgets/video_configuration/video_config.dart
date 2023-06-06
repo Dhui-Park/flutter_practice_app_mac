9c94de4c051b627088546ef48847497c26854e84
dhui <dhui@bagchaejin-ui-MacBookAir.local>
2023-06-06 15:15:54 +0900

    ValueNotifier

--- lib/features/common/widgets/main_navigation/widgets/video_configuration/video_config.dart
+++ lib/features/common/widgets/main_navigation/widgets/video_configuration/video_config.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 
-class VideoConfig extends ChangeNotifier {
-  bool autoMute = false;
+// ChangeNotifier은 리스트나 많은 메소드나 API등등을 쓰면 사용하는것이 좋다.
+// ValueNotifier은 ChangeNotifier의 기능을 똑같이 하지만 오직 하나의 값에만 사용 가능.
 
-  void toggleAutoMute() {
-    autoMute = !autoMute;
-    notifyListeners();
-  }
-}
-
-final videoConfig = VideoConfig();
+final videoConfig = ValueNotifier(false);
--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -27,12 +27,12 @@ class _SettingsScreenState extends State<SettingsScreen> {
       ),
       body: ListView(
         children: [
-          AnimatedBuilder(
-            animation: videoConfig,
-            builder: (context, child) => SwitchListTile.adaptive(
-              value: videoConfig.autoMute,
+          ValueListenableBuilder(
+            valueListenable: videoConfig,
+            builder: (context, value, child) => SwitchListTile.adaptive(
+              value: value,
               onChanged: (value) {
-                videoConfig.toggleAutoMute();
+                videoConfig.value = !videoConfig.value;
               },
               title: const Text('Auto Mute videos'),
               subtitle: const Text("Videos will be muted by default."),
